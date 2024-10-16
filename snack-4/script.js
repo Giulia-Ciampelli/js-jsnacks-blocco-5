33b9a5f0591b19fe520f0228e183848a923e0c09
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-16 15:32:34 +0200

    docs: use map method

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+let authors = posts.map(element => {
+  console.log(element.author);
+})
